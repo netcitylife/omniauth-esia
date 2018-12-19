fc8501d190deee5fa9711ffdf5898559426837ba
Архипов Дмитрий <da@netcitylife.ru>
2018-12-19 21:26:20 +0200

    Add additional resources (contacts,addresses,vehicles,kids,organizations)

--- lib/omniauth/strategies/esia.rb
+++ lib/omniauth/strategies/esia.rb
@@ -19,27 +19,16 @@ module OmniAuth
 
       uid { JWT.decode(access_token.token, nil, false).first['urn:esia:sbj_id'] }
 
-      info do
-        {
-          first_name:  raw_info['firstName'],
-          last_name:   raw_info['lastName'],
-          middle_name: raw_info['middleName'],
-          email:       raw_info['email']
-        }
-      end
-
-      extra do
-        {
-          raw_info: raw_info
-        }
+      def info
+        @info ||= get_resource
       end
 
       def authorize_params
         super.tap do |params|
-          params[:state] = state
-          params[:timestamp] = timestamp
-          params[:client_secret] = client_secret
-          params[:access_type] = options.access_type
+          params[:state]            = state
+          params[:timestamp]        = timestamp
+          params[:client_secret]    = client_secret
+          params[:access_type]      = options.access_type
           session['omniauth.state'] = state
         end
       end
@@ -48,52 +37,71 @@ module OmniAuth
         ::OAuth2::Client.new(options.client_id, client_secret, deep_symbolize(options.client_options))
       end
 
-      def raw_info
-        @raw_info ||= access_token.get("/rs/prns/#{uid}")&.parsed.merge!(get_email)
+      def get_contacts(ctt_id = nil)
+        get_resource('ctts', ctt_id)
       end
 
+      def get_addresses(addr_id = nil)
+        get_resource('addrs', addr_id)
+      end
+
+      def get_documents(doc_id = nil)
+        get_resource('docs', doc_id)
+      end
+
+      def get_vehicles(vhl_id = nil)
+        get_resource('vhls', vhl_id)
+      end
+
+      def get_organizations
+        get_resource('orgs')
+      end
+
+      def get_kids(kid_id = nil)
+        get_resource('kids', kid_id)
+      end
+
+      protected
+
       def build_access_token
-        code = request.params['code']
-        client.auth_code.get_token(code,
-          {
-            state: state,
-            scope: options.scope,
-            timestamp: timestamp,
-            redirect_uri: callback_url,
-            token_type: 'Bearer'
-          }
-        )
+        verifier = request.params["code"]
+        client.auth_code.get_token(verifier, {
+          state:        state,
+          scope:        options.scope,
+          timestamp:    timestamp,
+          redirect_uri: callback_url,
+          token_type:   'Bearer'
+        })
       end
 
       private
 
-        def client_secret
-          @client_secret ||= begin
-            data = "#{options.scope}#{timestamp}#{options.client_id}#{state}"
-            key  = OpenSSL::PKey.read(File.read(options.key_path))
-            crt  = OpenSSL::X509::Certificate.new(File.read(options.crt_path))
-            signed = OpenSSL::PKCS7.sign(crt, key, data, [], OpenSSL::PKCS7::DETACHED)
-            Base64.urlsafe_encode64(signed.to_der.to_s.force_encoding('utf-8'), padding: false)
-          end
+      def client_secret
+        @client_secret ||= begin
+          data   = "#{options.scope}#{timestamp}#{options.client_id}#{state}"
+          key    = OpenSSL::PKey.read(File.read(options.key_path))
+          crt    = OpenSSL::X509::Certificate.new(File.read(options.crt_path))
+          signed = OpenSSL::PKCS7.sign(crt, key, data, [], OpenSSL::PKCS7::DETACHED)
+          Base64.urlsafe_encode64(signed.to_der.to_s.force_encoding('utf-8'), padding: false)
         end
+      end
 
-        def state
-          @state ||= SecureRandom.uuid
-        end
+      def state
+        @state ||= SecureRandom.uuid
+      end
 
-        def timestamp
-          @timestamp ||= Time.now.strftime('%Y.%m.%d %H:%M:%S %z')
-        end
+      def timestamp
+        @timestamp ||= Time.now.strftime('%Y.%m.%d %H:%M:%S %z')
+      end
 
-        def get_email
-          { 'email' => access_token
-              .get("/rs/prns/#{uid}/ctts?embed=(elements)")
-              .parsed.fetch('elements', {})
-              .find { |e| e['type'] == 'EML' }
-              .fetch('value') }
-        rescue => e
-          {}
-        end
+      def get_resource(collection = nil, entity_id = nil, embed = true)
+        resource = ''
+        resource += "/#{collection}" if collection
+        resource += "/#{entity_id}" if entity_id
+        resource += '?embed=(elements)' if embed
+
+        access_token.get("/rs/prns/#{uid}#{resource}").parsed
+      end
     end
   end
 end
