cf3aa0f85b3574497531e2bc905bc010b8981e30
Архипов Дмитрий <da@netcitylife.ru>
2020-02-05 15:41:11 +0200

    Add ability to provide password for private key

--- lib/omniauth/strategies/esia.rb
+++ lib/omniauth/strategies/esia.rb
@@ -74,7 +74,7 @@ module OmniAuth
       def client_secret
         @client_secret ||= begin
           data   = "#{options.scope}#{timestamp}#{options.client_id}#{state}"
-          key    = options.key_path.is_a?(OpenSSL::PKey) ? options.key_path : OpenSSL::PKey.read(File.read(options.key_path), options.key_password)
+          key    = options.key_path.is_a?(OpenSSL::PKey::PKey) ? options.key_path : OpenSSL::PKey.read(File.read(options.key_path), options.key_password)
           crt    = options.crt_path.is_a?(OpenSSL::X509::Certificate) ? options.crt_path : OpenSSL::X509::Certificate.new(File.read(options.crt_path))
           signed = OpenSSL::PKCS7.sign(crt, key, data, [], OpenSSL::PKCS7::DETACHED)
           Base64.urlsafe_encode64(signed.to_der.to_s.force_encoding('utf-8'), padding: false)
